7789b52a2f833096d2f60b38130335aebfd6203c
Dominic Jones-Jackson <sethveeper@gmail.com>
2020-09-27 10:05:19 -0500

    Updated CountingSort
    Just some tweaks, which proved a little more frustrating than necessary, but it's done now.

--- ProjectMakeItSo/CountingSort/Program.cs
+++ ProjectMakeItSo/CountingSort/Program.cs
@@ -7,40 +7,68 @@ namespace CountingSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Fox. Let's do a Counting sort!");
+            Console.WriteLine("Hello, Fox. Let's do some Counting sorts!");
+            Console.WriteLine("We can do as many as you want, and they can be as big as you want.");
 
-            Console.WriteLine("First, we need an array of numbers. Please input the Maximum Value that array should contain: ");
-            int max = GetInput();
-
-            Console.WriteLine("Now we need to get an array...");
-
-            Console.WriteLine("Please input the number of values it should contain: ");
-            int length = GetInput();
-            int[] array = GetArray(max, length);
-
-            Console.WriteLine("Here it is unsorted!");
-            foreach (int item in array)
+            bool exit = false;
+            while (!exit)
             {
-                Console.WriteLine("- " + item.ToString());
+                Console.WriteLine("First, we need an array of numbers. Please input the Maximum Value that array should contain: ");
+                int max = GetInt();
+
+                Console.WriteLine("Now we need to get an array...");
+
+                Console.WriteLine("Please input the number of values it should contain: ");
+                int length = GetInt();
+                int[] array = GetArray(max, length);
+
+                GetContinue("Press enter to see the unsorted list.");
+
+                Console.WriteLine("Here it is unsorted!");
+                foreach (int item in array)
+                {
+                    Console.WriteLine("- " + item.ToString());
+                }
+                // End of foreach (Printing sorted array)
+
+
+                GetContinue("Press enter when you're ready to sort this!");
+                array = Sort(array, max);
+                // Don't forget to change this back!
+
+                Console.WriteLine("Here it is sorted!");
+
+                foreach (int item in array)
+                {
+                    Console.WriteLine("- " + item.ToString());
+                }
+                // End of foreach
+
+                exit = GetExit();
             }
-            // End of foreach
-
-            Console.WriteLine("Now we're going to sort them!");
-
-            array = Sort(array, max);
-
-            Console.WriteLine("Here it is sorted!");
-            foreach (int item in array)
-            {
-                Console.WriteLine("- " + item.ToString());
-            }
-            // End of foreach
+            // End of main while loop
         }
         // End of Main method
 
         public static int[] Sort(int[] array, int max)
         {
-            int[] bucket = new int[max];
+            int[] bucket = new int[max + 1];
+            /* Please don't ask why "max" is incremented. 
+            I don't want to talk about it.
+            
+            Anyways. We'll use this to help sort the values we're getting.
+            Each index indicates the number being sorted, and the value
+            indicates how many of that number we've encountered so far.
+            I'm calling it "bucket" because I like to visualize each of those 
+            indexes as a little bucket, slowly filling up with a specific value.
+
+            I do wonder, though... It *might* be more efficient if, rather than 
+            setting a "bucket" for each *possible* number, we could simply add 
+            buckets as we go and only have buckets for each unique value we 
+            actually find? However, that starts to wander away from the 
+            original Counting Sort, so I guess we won't do that today.
+            */
+
             for (int i = 0; i < bucket.Length; i++)
             {
                 bucket[i] = 0;
@@ -54,14 +82,23 @@ namespace CountingSort
             // End of for loop (Sorting into Buckets)
 
             int index = 0;
+            // We'll use this in a minute to keep track 
+            // of how far into the array we've gotten.
             for (int i = 0; i < bucket.Length; i++)
             {
                 Console.WriteLine("Sorting " + bucket[i] + " total " + i + "s...");
+                // First, we iterate though the buckets...
 
                 for (int j = 0; j < bucket[i]; j++)
                 {
+                    // Each bucket's index knows how many of that value 
+                    // was encountered in the array.
+
                     Console.WriteLine("- Index[" + (j + index) + "] gets a " + i);
                     array[j + index] = i;
+                    // So, we add the current bucket index to the array, 
+                    // a number of times equal to the value stored in it.
+
                 }
                 // End of for loop (Inner)
                 index += bucket[i];
@@ -78,17 +115,22 @@ namespace CountingSort
             int[] output = new int[length];
             Random random = new Random();
 
-            for (int i = 0; i < output.Length; i++)
+            output[0] = max;
+            // The first one will *definitely* be whatever the maximum value is.
+            // No reason. Just felt like it.
+
+            for (int i = 1; i < (output.Length); i++)
             {
                 output[i] = random.Next(0, max);
             }
-            // End of For loop
+            // End of For loop (Assigning random values)
+
 
             return output;
         }
         // End of GetArray method
 
-        public static int GetInput()
+        public static int GetInt()
         {
             int output = 0;
             string input;
@@ -111,11 +153,48 @@ namespace CountingSort
             }
             // End of while loop (Validate Input)
 
-            Console.WriteLine("Congratulations! You input " + output.ToString());
+            Console.WriteLine("That works! You input " + output.ToString() + ".");
 
             return output;
         }
-        // End of GetMax method
+        // End of GetInt method
+
+        public static bool GetExit()
+        {
+            string input = "";
+            bool output = false;
+            while (input == "")
+            {
+                Console.WriteLine("Would you like to go again? [y/n]");
+                input = Console.ReadLine();
+
+                if (input == "n")
+                {
+                    Console.WriteLine("Alright, bye for now!");
+                    output = true;
+                }
+                else if (input == "y")
+                {
+                    Console.WriteLine("Alright, we can do another one!");
+                }
+                else
+                {
+                    Console.WriteLine("... Sorry? Please give either a 'y' or a 'n'...");
+                    input = "";
+                }
+                // End of if/else if/else statement
+            }
+            // End of while loop (Validate input)
+            return output;
+        }
+        // End of GetExit method
+
+        public static void GetContinue(string prompt)
+        {
+            Console.WriteLine(prompt);
+            Console.ReadKey();
+        }
+        // End of GetExit method
     }
     // End of Program Class
 }
