9e7f08822dbfcb414b217911fe19782ba0f15f7d
Dominic Jones-Jackson <44783114+sethveeper@users.noreply.github.com>
2020-10-26 15:26:20 -0500

    Updated Slightly
    We're eventually going to build this into a "red/black" self balancing tree. Right now it's just a bit of set up and basic functionality, however.

--- ProjectMakeItSo/BinTree/Branch.cs
+++ ProjectMakeItSo/BinTree/Branch.cs
@@ -11,31 +11,60 @@ namespace BinTree
     {
         #region Properties and Constructors
         private byte value;
+        private bool red;
+        private Branch prev;
         private Branch less;
         private Branch more;
 
         public byte Value { get => value; set => this.value = value; }
+        public bool Red { get => red; set => red = value; }
+        public Branch Prev { get => prev; set => prev = value; }
         public Branch Less { get => less; set => less = value; }
         public Branch More { get => more; set => more = value; }
+
         // End of Properties
 
         public Branch(byte value)
         {
             this.value = value;
+            this.red = false;
+            this.prev = null;
             this.less = null;
             this.more = null; 
         }
         // End of Value constructor
-
-        public Branch()
-        {
-            this.value = 0;
-            this.less = null;
-            this.more = null;
-        }
-        // End of Default constructor
         #endregion
         // End of Properties and Constructors
+
+        public void Add(byte input)
+        {
+            if (this.value > input)
+            {
+                if (this.less != null)
+                    this.less.Add(input);
+                else
+                    this.less = new Branch(input);
+            }
+            // End of If (This value is greater than the input)
+            else
+            {
+                if (this.more != null)
+                    this.more.Add(input);
+                else
+                    this.more = new Branch(input);
+            }
+            // End of If (This value is less than or equal to the input)
+        }
+        // End of Add method
+
+        public void Switch()
+        {
+            if (this.red)
+                this.red = false;
+            else
+                this.red = true;
+        }
+        // End of Switch method
 
         public override string ToString()
         {
