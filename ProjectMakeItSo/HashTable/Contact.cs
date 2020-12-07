6a489c1b49f4ddcdeccb79ce2e4363ef3e61a251
Dominic Jones-Jackson <sethveeper@gmail.com>
2020-12-07 12:36:29 -0600

    Finished HashTable final project
    Just need to write some documentation for it in class today.

---
+++ ProjectMakeItSo/HashTable/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    class Contact
+    {
+        private string name;
+        private string phone;
+
+        public string Name { get => name; set => name = value; }
+        public string Phone { get => phone; set => phone = value; }
+
+        public Contact(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+        // End of Constructor
+
+        public Contact()
+        {
+            this.name = "Optic";
+            this.phone = "098-765-4321";
+        }
+        // End of Default Constructor
+
+        public override string ToString()
+        {
+            string output = "";
+
+            output += "Name: " + this.name + "\n";
+            output += "Contact: " + this.phone + "\n";
+
+            return output;
+        }
+        // End of ToString method
+    }
+    // End of Contact class
+}
+// End of namespace
